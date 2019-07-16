1ae76d1a920c48bf3085ffdcd67ac065adf998aa
Daniel Fontes <daniellexj@gmail.com>
2019-07-15 22:00:43 -0300

    Corracoes: update em categoriaService e construtor de Pagamento

--- src/main/java/com/daniel/cursomc/services/CategoriaService.java
+++ src/main/java/com/daniel/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.daniel.cursomc.domain.Categoria;
+import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.dto.CategoriaDTO;
 import com.daniel.cursomc.repositories.CategoriaRepository;
 import com.daniel.cursomc.services.exception.DataIntegrityException;
@@ -33,9 +34,15 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 
-	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+	public Categoria update(Categoria obj) {		
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());		
+		
 	}
 	
 	public void delete (Integer id) {
