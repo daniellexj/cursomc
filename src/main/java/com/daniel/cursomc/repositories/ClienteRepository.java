e2a523d9fe672d7f1d72c08de3ddc311d6158cd6
Daniel Fontes <daniellexj@gmail.com>
2019-07-16 22:48:27 -0300

    validacao customizada: email não repetido na insercao de cliente

--- src/main/java/com/daniel/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/daniel/cursomc/repositories/ClienteRepository.java
@@ -1,10 +1,13 @@
 package com.daniel.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import com.daniel.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+	
+	@Transactional(readOnly = true)	
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/daniel/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/daniel/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.domain.enums.TipoCliente;
 import com.daniel.cursomc.dto.ClienteNewDTO;
+import com.daniel.cursomc.repositories.ClienteRepository;
 import com.daniel.cursomc.resources.exception.FieldMessage;
 import com.daniel.cursomc.services.validation.utils.BR; 
  
 public class ClienteInsertValidator implements ConstraintValidator < ClienteInsert , ClienteNewDTO> { 
  
+	@Autowired
+	private ClienteRepository repo;
+	
     @Override 
     public void initialize(ClienteInsert ann) {
     	}
@@ -28,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator < ClienteInse
         
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && ! BR.isValidCNPJ(objDto.getCpfOUcnpj())) {
         	list.add(new FieldMessage("cpfOUcnpj ","CNPJ invalido"));
+        }
+        
+        Cliente axu = repo.findByEmail(objDto.getEmail());
+        
+        if (axu != null) {
+        	list.add(new FieldMessage("email ","E-mail ja cadastrado"));
         }
 
         // inclua os testes aqui, inserindo erros na lista
